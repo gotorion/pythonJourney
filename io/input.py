064114220b6e0fdada768189a8931f99a7c7a6a2
Junhui Li <junhui.lee@icloud.com>
2024-06-27 23:43:30 +0800

    add: input and output

---
+++ io/input.py
@@ -0,0 +1,6 @@
+str = input("Please input: ")
+print(str)
+
+num = input('Please input num: ')
+num = int(num)
+print('num =', num)
---
+++ io/print.py
@@ -0,0 +1,17 @@
+print("Hello")
+print('Hello')
+print("""Hello""")
+
+num = 10
+print(num)
+print(num + 10)
+
+print(num, 50, sep = '|', file = None)
+
+fp = open('file.txt', 'w')
+# print("Hello world", "This is test file", sep = '\n', end = ">>>", file=fp)
+print("Hello" + "World") # connect string
+
+"""
+doc string
+"""
